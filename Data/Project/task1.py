b08b276cd34c47a66f4d925dcda2c4ce493f8549
Vincenzo Lombardi <vincenzolombardi99@gmail.com>
2024-12-02 16:31:58 +0100

    Added the Hessians to the FRA dynamic

--- Data/Project/task1.py
+++ Data/Project/task1.py
@@ -8,12 +8,14 @@ from numpy import *
 dt = dtCollection.task0_discretizationStep;
 T = TCollection.task1_trajectoryDuration;
 
-TT = int(T/dt) # number of time steps (each one of duration dt in order to evolve from t=0 to t=T)
+TT = int(T/dt) # number of time steps (each one of duration dt, enough for evolve from t=0 to t=T)
 
-# Definition of the desired trajectory
+# Definition of the desired trajectory (step from eq. point [0, 0, 0, 0]' to eq. point [pi, 0, 0, 0]')
 xx_des = zeros((ns, TT))
 uu_des = zeros((ni, TT))
 xx_des[0, int(TT/2):] = ones((1, int(TT/2)))*pi
+
+# Notice: first of all, is needed the definition of a cost function l (with its jacobian and hessian wrt x and u)
 
 # Now implements (in another file) N.M. with Armijo's rule, regularized, closed-loop version, in order to get a feasible opt trj.
 # At each iteration, evaluate dynamic, solve lambda co-state equation, compute descendent direction solving a LQOCP (considering
