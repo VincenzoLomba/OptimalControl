b7f04650704c6a2ba372a80ac5569d9402d4fb30
circuito28 <letti.alessandro@gmail.com>
2024-12-08 23:30:56 +0100

    useful_functions.py

--- task1/useful_functions.py
+++ task1/useful_functions.py
@@ -16,73 +16,32 @@ def is_pos_def(M, tol=1e-8):
         la.LinAlgError
         return False
     
-def stepsize_armijo(stepsize_0, armijo_maxiters, cc, beta, deltau, xx_ref, uu_ref,  x0, uu, JJ, descent_arm):
+# TRAJECTORIES GENERATORS:
 
-      """
-      Computes the stepsize using Armijo's rule.
-      input parameters:
-            - stepsize_0 : initial stepsize guess,
-            - armijo_maxiters : maximum number of iterations for armijo rule,
-            - deltau : descending direction for the control action,
-            - xx_ref : reference trajectory state,
-            - uu_ref : reference trajectory input,
-            - x0 : initial state,
-            - uu : input at current iteration,
-            - JJ : cost at current iteration,
-            - descent_arm: armijo descent direction at current iteration,
-            
-      output parameters:
-            - stepsize
-      """
+#1) STEP/RAMP GENERATOR
+def traj_gen(step_reference, tf, dt, ns, ni):
 
-      TT = uu.shape[1]
+      TT = int(1/dt)
+      #TT = int(100)
+      
+      ref_deg_0 = 0
+      ref_deg_T = 30
 
+      
+      xx_ref = np.zeros((ns, TT))
+      uu_ref = np.zeros((ni, TT))
 
-      stepsizes = []  # list of stepsizes
-      costs_armijo = []
+      
+      if not step_reference:
+            # In this case we generate a ramp along all the time horizon
+            xx_ref[0,:] = np.linspace(np.deg2rad(ref_deg_0), np.deg2rad(ref_deg_T), TT)
+            uu_ref[0,:] = np.sin(xx_ref[0,:])
 
-      stepsize = stepsize_0
-
-      ns = xx_ref.shape[0]
-      ni = uu_ref.shape[0]
-
-
-      for ii in range(armijo_maxiters):
-
-            # temp solution update
-
-            xx_temp = np.zeros((ns,TT))
-            uu_temp = np.zeros((ni,TT))
-
-            xx_temp[:,0] = x0
-
-
-            for tt in range(TT-1):
-                  uu_temp[:,tt] = uu[:,tt] + stepsize*deltau[:,tt]
-                  xx_temp[:,tt+1] = discretizedDynamicFRA(xx_temp[:,tt], uu_temp[:,tt])[0].squeeze()
-
-            # temp cost calculation
-            JJ_temp = 0
-
-            for tt in range(TT-1):
-                  temp_cost = stagecost(xx_temp[:,tt], uu_temp[:,tt], xx_ref[:,tt], uu_ref[:,tt])[0]
-                  JJ_temp += temp_cost
-
-            temp_cost = termcost(xx_temp[:,-1], xx_ref[:,-1])[0]
-            JJ_temp += temp_cost
-
-            stepsizes.append(stepsize)      # save the stepsize
-            costs_armijo.append(np.min([JJ_temp, 100*JJ]))    # save the cost associated to the stepsize
-
-            if JJ_temp > JJ  + cc*stepsize*descent_arm:
-                  # update the stepsize
-                  stepsize = beta*stepsize
-
-            else:
-                  print('Armijo stepsize = {:.3e}'.format(stepsize))
-                  break
-            
-            if ii == armijo_maxiters -1:
+      else:
+            xx_ref[0,int(TT/2):] = np.ones((1,int(TT/2)))*np.ones((1,int(TT/2)))*np.deg2rad(ref_deg_T)
+            uu_ref[0,:] = np.sin(xx_ref[0,:])
+   
+      return xx_ref, uu_ref
                   print("WARNING: no stepsize was found with armijo rule!")
 
       return stepsize
