a98e765040044f2f2d9d7be791c431b1e309f473
Vincenzo Lombardi <vincenzolombardi99@gmail.com>
2024-12-26 19:40:38 +0100

    Moving tasks files and adding Task3 code

--- Data/Project/main.py
+++ Data/Project/main.py
@@ -5,8 +5,9 @@ from parameters import *
 from numpy import *
 from trajectories import *
 from methods import *
-from task1 import task1
-from task2 import task2
+from tasks.task1 import task1
+from tasks.task2 import task2
+from tasks.task3 import task3
 
 # Tasks selection
 tasks = [0, 1, 0, 0]
@@ -16,22 +17,9 @@ tasks = [None] + tasks
 print(tasks)
 if (tasks[1]): task1()
 if (tasks[2]): task2()
+if (tasks[3]): task3()
 
 """
-
-############################
-########## TASK 3 ##########
-############################
-
-if(task[3]):
-    # Computation of local linearization around optimal trajectory and consequently LQR gain KK
-    KK = ComputeLocalLin(xx_opt, uu_opt, QQ, RR, QQ, TT, discretizedDynamicFunction, solveLinearLQP)[0]
-
-    # Design of the LQR with noise
-    noise = GenerateNoise(xx_opt, noise_std_percentage=0.2)
-    xx_track, uu_track = SolveLQPwithNoise(xx_opt, uu_opt, KK, noise, TT, discretizedDynamicFunction, False)
-    xx_track, uu_track = SolveLQPwithNoise(xx_opt, uu_opt, KK, noise, TT, discretizedDynamicFunction, True)
-
 
 ############################
 ########## TASK 4 ##########
